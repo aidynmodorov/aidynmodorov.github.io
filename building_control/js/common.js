f9935a8a91c7873cbf1a9d8b0b0e665669a302b5
aidynmodorov <aidynmodorov@gmail.com>
2018-05-05 01:40:03 +0700

    commit

--- building_control/js/common.js
+++ building_control/js/common.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
 
+	$("#phone").mask("+7 (999) 999-9999");
+	$("#phone1").mask("+7 (999) 999-9999");
+	$("#phone2").mask("+7 (999) 999-9999");
+	$("#phone3").mask("+7 (999) 999-9999");
+	$("#phone4").mask("+7 (999) 999-9999");
+
 	$(window).scroll(function() {
 		if ($(this).scrollTop() > $(this). height()) {
 			$('.main-sticky_nav').fadeIn();
@@ -7,6 +13,8 @@ $(document).ready(function() {
 			$('.main-sticky_nav').fadeOut();
 		}
 	});
+
+	setInterval(function(){$('.button').toggleClass('active');}, 2000);
 
 	var menuBtn = $('.hamburger');
 	var menu = $('.main-mobnav');
@@ -16,9 +24,30 @@ $(document).ready(function() {
   	menuBtn.toggleClass('hamburger is-active');
   });
 
+  $('.mobmenu a').click(function() {
+  	menuBtn.toggleClass('hamburger is-active');
+  	menu.toggleClass('main-mobnav__active');
+  })
+
   $('.packages-item p').equalHeights();
 
-  $('.reviews-slider').slick({
+  $("#services").viewportChecker({
+		callbackFunction:function(){
+		$('.services-item').each(function(i) {
+	  	$(this).delay((i++) * 700).fadeTo(1000, 1);
+	  });
+		}
+	});
+
+	$("#steps").viewportChecker({
+		callbackFunction:function(){
+		$('.steps-item').each(function(i) {
+	  	$(this).delay((i++) * 700).fadeTo(1000, 1);
+	  });
+		}
+	});
+
+	$('.reviews-slider').slick({
   	slidesToShow: 3,
   	slidesToScroll: 1,
   	prevArrow: '<button type="button" class="reviews-slider-btn reviews-prev-btn"><img src="img/icons/left-arrow.svg" alt=""/></button>',
@@ -37,6 +66,7 @@ $(document).ready(function() {
 		      settings: {
 		      	slidesToShow: 1,
 		        slidesToScroll: 1,
+		        adaptiveHeight: true
 		      }
 		    }
 		  ]
@@ -67,9 +97,35 @@ $(document).ready(function() {
 	 	$('#premium-order').fadeIn();
 	});
 
+	$(document).mouseup(function (e){
+		var div = $('.popup-content');
+		if (!div.is(e.target)
+			&& div.has(e.target).length === 0) {
+			$('.popup').fadeOut();
+	}
+	});
+
 	$('.popup-close').on('click', function(event) {
 	 	event.preventDefault();
 	 	$('.popup').fadeOut();
+	});
+
+	$('.policy-popup').on('click', function(event) {
+		event.preventDefault();
+	 	$('.policy').fadeIn();
+	});
+
+	$('.policy-close').on('click', function(event) {
+	 	event.preventDefault();
+	 	$('.policy').fadeOut();
+	});
+
+	$(document).mouseup(function (e){
+		var div = $('.policy-content');
+		if (!div.is(e.target)
+			&& div.has(e.target).length === 0) {
+			$('.policy').fadeOut();
+	}
 	});
 
 	//E-mail Ajax Send
@@ -82,7 +138,7 @@ $(document).ready(function() {
 		}).done(function() {
 				$('.popup-content').find('.popup-success').addClass('active').css('display', 'flex').hide().fadeIn();
 			setTimeout(function() {
-				$('.popup-content').find('.success-popup').removeClass('active').fadeOut();
+				$('.popup-content').find('.popup-success').removeClass('active').fadeOut();
 				$('.popup').fadeOut();
 				th.trigger("reset");
 			}, 3000);
